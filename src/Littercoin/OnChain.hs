563d8e9c5d2b0c2cf39c4d3a55efd58a817d84dc
lley154 <lawrence.ley@contextsolutions.ca>
2022-07-25 07:48:54 -0400

    - inital setup and import pruning

--- src/Littercoin/OnChain.hs
+++ src/Littercoin/OnChain.hs
@@ -1,67 +1,85 @@
-{-# LANGUAGE DataKinds         		#-}
-{-# LANGUAGE NoImplicitPrelude 		#-}
-{-# LANGUAGE TemplateHaskell 		#-}
-{-# LANGUAGE ScopedTypeVariables	#-}
-{-# LANGUAGE TypeApplications    	#-}
-{-# LANGUAGE ImportQualifiedPost	#-}
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DeriveAnyClass        #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NamedFieldPuns        #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE RankNTypes            #-}
+{-# LANGUAGE RecordWildCards       #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
 
-module Littercoin.OnChain 
-  ( serialisedScript,
-    scriptSBS,
-    writeSerialisedScript,
+module Littercoin.OnChain
+  ( --curSymbol
+   policy
   )
 where
 
 
-
-import Cardano.Api (PlutusScriptV2, writeFileTextEnvelope)
-import Cardano.Api.Shelley (PlutusScript (..))
-import Codec.Serialise
-import Data.Functor (void)
-import PlutusTx qualified
-import PlutusTx.Prelude qualified
-import Plutus.V2.Ledger.Api qualified as PlutusV2
---import Plutus.Script.Utils.V2.Scripts as Utils
-import Plutus.Script.Utils.V2.Typed.Scripts.Validators as Utils
---import Ledger.Address as Addr
-import Data.ByteString.Lazy qualified as LBS
-import Data.ByteString.Short qualified as SBS
-import Prelude (IO, (.), ($), Maybe(Nothing))
-
-
-
-newtype MyCustomDatum = MyCustomDatum PlutusTx.Prelude.Integer
-PlutusTx.unstableMakeIsData ''MyCustomDatum
-newtype MyCustomRedeemer = MyCustomRedeemer PlutusTx.Prelude.Integer
-PlutusTx.unstableMakeIsData ''MyCustomRedeemer
-
--- This validator always validates true
-{-# INLINABLE mkValidator #-}
-mkValidator :: MyCustomDatum -> MyCustomRedeemer -> PlutusV2.ScriptContext -> PlutusTx.Prelude.Bool
-mkValidator _ _ _ = PlutusTx.Prelude.True
-
-validator :: PlutusV2.Validator
-validator = PlutusV2.mkValidatorScript
-    $$(PlutusTx.compile [|| wrap ||])
- where
-    wrap = Utils.mkUntypedValidator mkValidator
+import qualified Ledger
+import qualified Ledger.Typed.Scripts           as Scripts
+import qualified Ledger.Value                   as Value
+import           Littercoin.Types               (MintPolicyRedeemer(..), MintPolicyParams(..))
+import qualified Plutus.Script.Utils.V2.Typed.Scripts.MonetaryPolicies as MP
+import qualified Plutus.V2.Ledger.Api           as PlutusV2
+import qualified Plutus.V2.Ledger.Contexts      as ContextV2
+import qualified PlutusTx                       (applyCode, compile, liftCode)
+import           PlutusTx.Prelude               (Bool(..), traceIfFalse, (==), (&&), (>=),($))
 
 
 {-
-   As a Short Byte String
+   The policy
 -}
 
-scriptSBS :: SBS.ShortByteString
-scriptSBS = SBS.toShort . LBS.toStrict $ serialise $ validator
+{-# INLINABLE mkPolicy #-}
+mkPolicy :: MintPolicyParams -> MintPolicyRedeemer -> PlutusV2.ScriptContext -> Bool
+mkPolicy params (MintPolicyRedeemer polarity) ctx = 
+
+   if polarity then
+      traceIfFalse "mkPolicy: invalid admin signature" signedByAdmin
+      && traceIfFalse "mkPolicy: invalid NFT params" validTokenName
+   else            
+      traceIfFalse "mkPolicy: invalid NFT params" validTokenName
+
+   where
+      pkh :: PlutusV2.PubKeyHash
+      pkh = mpAdminPkh params
+
+      tn :: PlutusV2.TokenName
+      tn = mpTokenName params
+
+      info :: PlutusV2.TxInfo
+      info = PlutusV2.scriptContextTxInfo ctx  
+
+      signedByAdmin :: Bool
+      signedByAdmin =  ContextV2.txSignedBy info pkh
+
+      -- Validate the correct token name that is being minted or burned
+      validTokenName :: Bool
+      validTokenName = case Value.flattenValue (PlutusV2.txInfoMint info) of
+         [(cs, tn', amt)] -> cs == ContextV2.ownCurrencySymbol ctx && tn' == tn && amt >= 1
+         _                -> False
+ 
 
 {-
-   As a Serialised Script
+    As a Minting Policy
 -}
 
-serialisedScript :: PlutusScript PlutusScriptV2
-serialisedScript = PlutusScriptSerialised scriptSBS
 
-writeSerialisedScript :: IO ()
-writeSerialisedScript = void $ writeFileTextEnvelope "OnChain.plutus" Nothing serialisedScript
+-- | Wrap the minting policy using the boilerplate template haskell code
+policy :: MintPolicyParams -> Scripts.MintingPolicy
+policy mp = PlutusV2.mkMintingPolicyScript $
+    $$(PlutusTx.compile [|| \mp' -> MP.mkUntypedMintingPolicy $ mkPolicy mp' ||])
+    `PlutusTx.applyCode`
+     PlutusTx.liftCode mp
 
 
+-- | Provide the currency symbol of the minting policy which requires MintPolicyParams
+--   as a parameter to the minting policy
+curSymbol :: MintPolicyParams -> Value.CurrencySymbol
+curSymbol mpParams = Ledger.scriptCurrencySymbol $ policy mpParams 
