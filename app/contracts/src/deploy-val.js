8003d878a929cbae2ccfc615beefec5b01bdb304
lley154 <lawrence.ley@contextsolutions.ca>
2022-12-22 07:59:31 -0500

    - replacing symlinks with deployment src directory

---
+++ app/contracts/src/deploy-val.js
@@ -0,0 +1,24 @@
+import * as helios from "./helios.js"
+
+const lcValidatorSrc = await Deno.readTextFile("./src/lcValidator.hl");
+const programLC = helios.Program.new(lcValidatorSrc);
+const simplify = false;
+const lcUplcProgramLC = programLC.compile(simplify);
+const lcDatum = programLC.evalParam("LC_DATUM");
+const lcDatumData = lcDatum.data;
+const valAddAdaRedeemer = programLC.evalParam("VAL_ADD_ADA_REDEEMER");
+const valAddAdaRedeemerData = valAddAdaRedeemer.data;
+const valMintRedeemer = programLC.evalParam("VAL_MINT_REDEEMER");
+const valMintRedeemerData = valMintRedeemer.data;
+const valBurnRedeemer = programLC.evalParam("VAL_BURN_REDEEMER");
+const valBurnRedeemerData = valBurnRedeemer.data;
+
+const vHash = lcUplcProgramLC.validatorHash;
+console.log("littercoin validator hash: ", vHash.hex);
+
+await Deno.writeTextFile("./deploy/redeemer-add-ada.json", valAddAdaRedeemerData.toSchemaJson());
+await Deno.writeTextFile("./deploy/redeemer-val-mint.json", valMintRedeemerData.toSchemaJson());
+await Deno.writeTextFile("./deploy/redeemer-val-burn.json", valBurnRedeemerData.toSchemaJson());
+await Deno.writeTextFile("./deploy/lc-datum-init.json", lcDatumData.toSchemaJson());
+await Deno.writeTextFile("./deploy/lc-validator.plutus", lcUplcProgramLC.serialize());
+await Deno.writeTextFile("./deploy/lc-validator.hash", vHash.hex);
